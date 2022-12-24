d52fade68ef7efc5eeb1df429e0400d7b7eabcf4
Samuel <samuellage21@gmail.com>
2022-12-24 11:47:41 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag img
+  // const img = document.querySelector("#profile img")
+
+  // //substituir a imagem 
+  // if(html.classList.contains('light')) {
+  //   //se estiver light mode, addicionar a imagem light
+  //   img.setAttribute('src', '/.assets/avatar-light.jng')
+  // }else{
+  //     //se estiver sem light mode, manter imagem normal
+  //   img.setAttribute('src', '/.assets/avatar.jng')
+  // }
+
+}
