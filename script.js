78bf4480d7cfb9a24ac42ffcfb3c0e51cdf38b39
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-04-07 19:30:56 -0300

    Add. frameworks e implementando tooltips

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+(() => {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+})();
+
 function toggleMode() {
   const html = document.documentElement
   html.classList.toggle("light")
-
+  
   //pegar a tag img
   // const img = document.querySelector("#profile img")
-
+  
   // //substituir a imagem 
   // if(html.classList.contains('light')) {
   //   //se estiver light mode, addicionar a imagem light
@@ -13,5 +19,5 @@ function toggleMode() {
   //     //se estiver sem light mode, manter imagem normal
   //   img.setAttribute('src', '/.assets/avatar.jng')
   // }
-
+  
 }
